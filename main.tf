80438d833d2dce411baf522fb8b112d25ac5f65e
posmackenzie <118050166+posmackenzie@users.noreply.github.com>
2022-11-25 21:31:25 -0300

    Change main

--- main.tf
+++ main.tf
@@ -24,9 +24,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "random_pet" "sg" {}
-
-
 resource "aws_instance" "web" {
   ami                    = "ami-09d3b3274b6c5d4aa"
   instance_type          = "t2.micro"
